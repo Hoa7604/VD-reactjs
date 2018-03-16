f1b8e195969de557ce225c580098b32cc0948a31
Vo Hoa <12520147@gm.uit.edu.vn>
2018-03-16 17:05:52 +0700

    add file react 16/03

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React, { Component } from 'react';
-//import logo from './logo.svg';
-import './App.css';
 import Main from './hover-effect-component/Main';
 
 class App extends Component {
   render() {
     return (  
         <div>
-            <h1>show react</h1>
             <Main />
         </div>
     );
--- src/hover-effect-component/Main.js
+++ src/hover-effect-component/Main.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
+// import './style.css';
 
 class Main extends Component {
   render() {
     return (
     	<div>
+    		<title>hover-effect</title>
     		<ul>
     			<li>1</li>
     			<li>2</li>
     			<li>3</li>
     			<li>4</li>
     		</ul>
-
     	</div>
     );
   }
