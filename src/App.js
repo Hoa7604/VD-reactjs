23b84f52853db3e177f8710daefa7b4eccafe87b
Vo Hoa <12520147@gm.uit.edu.vn>
2018-03-20 15:10:46 +0700

    add doccument react 20/03

--- src/App.js
+++ src/App.js
@@ -1,11 +1,74 @@
 import React, { Component } from 'react';
+import './App.css';
 import Main from './hover-effect-component/Main';
 
 class App extends Component {
+    ShowInfoHuman(human)
+    {
+        if (human.status) {
+            return <h1>
+                        {human.id } <br/>
+                        {human.name} <br/>
+                        {human.address} <br/>
+                        status : {human.status ? 'entire' : 'propose'}
+                    </h1>
+        }
+    }
   render() {
-    return (  
+    var a = 5.3;
+    var b = 'reactJS';
+    var c  = 10;
+    var human = {
+        id : 1,
+        name : 'Hoa',
+        address : 3256,
+        status : true
+    };
+
+    var users = [
+        {
+            id : 1,
+            name : 'Han',
+            address : 15212
+        },
+        {
+            id : 2,
+            name : 'hieu',
+            address : 25611
+        },
+        {
+            id : 3,
+            name : 'hong',
+            address : 2353
+        }
+    ];
+
+    var elements = users.map((user) => {
+        return  <div key = {user.id}>
+                    <h3>name : { user.name }</h3>
+                    <span>address : {user.address}</span>
+                </div>
+    });
+
+    return (   
         <div>
             <Main />
+            <div>
+                <h2>
+                    a : {a} <br/>
+                    b : {b} <br/>
+                    c : {c} <br/>
+                    d : {a + c} <br/>
+                </h2>
+            </div>
+            <div>
+                <h1>
+                    {human.id } <br/>
+                </h1>
+                {this.ShowInfoHuman(human)}
+                <br/>
+                { elements }
+            </div>
         </div>
     );
   }
